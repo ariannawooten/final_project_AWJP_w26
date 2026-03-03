5e3e9f38c53f773f78afac8986272e602fe19bbe
Arianna Wooten <ariannasw@uchicago.edu>
2026-03-02 22:22:31 -0600

    created transportation burden bar plot, cleaned it up, commented out other plots, removed dashboard columns so there's one single column, and tentatively updated the sidebar options

--- code/jpaw_app.py
+++ code/jpaw_app.py
@@ -9,41 +9,48 @@ import altair as alt
 import json
 
 # load data
-
-cpum = pd.read_csv('/Users/ariannawooten/Downloads/final_project_AWJP_w26/data/raw-data/meps_00001.csv.gz') # CPUM national health data
 cha = pd.read_csv('/Users/ariannawooten/Downloads/final_project_AWJP_w26/data/raw-data/Chicago Health Atlas Data Download - Census Tracts.csv')#chicago health atlas data (2020)
 
 # remove first 3 rows, which have data definitions, citations, etc.
 cha = cha.iloc[4:809]
 
-# create histogram
+# create histogram: proximity to roads, railways, and airports by census
+#hist = alt.Chart(cha).mark_bar().encode(
+#    alt.X('Name:N', title='Census Tract', 
+#    sort = alt.EncodingSortField(field='EKR_2024', order = 'descending')),
+#    alt.Y('average(EKR_2024):Q', title = 'Proximity to Roads, Railways, and Airports')
+#    )
 
-hist = alt.Chart(cha).mark_bar().encode(
-    alt.X('Name:O').bin(),
-    alt.Y('average(EKR_2024)')
-)
+#hist
 
-hist
+#scatter = alt.Chart(cha).mark_point().encode(
+#    alt.X('Name:O'),
+#    alt.Y('EKR_2024')
+#)
 
-scatter = alt.Chart(cha).mark_point().encode(
-    alt.X('Name:O'),
-    alt.Y('EKR_2024')
-)
+#scatter
 
-scatter
+# bar plot 2: median household income by census tract
+#hist2 = alt.Chart(cha).mark_bar().encode(
+#    alt.X('Name:O', title='Census Tract', sort = alt.EncodingSortField(field='INC_2020-2024', order = 'descending')),
+#    alt.Y('INC_2020-2024:Q', title='Median Household Income')
+#)
 
-hist = alt.Chart(cha).mark_bar().encode(
-    alt.X('Name:O'),
-    alt.Y('INC_2020-2024:Q')
-)
+#hist2
 
-hist
+# transportation burden plot
+#transpo = alt.Chart(cha).mark_bar().encode(
+#    alt.X('Name:O', title='Census Tract', sort = alt.EncodingSortField(field='RITB_2022', order = 'descending')),
+#    alt.Y('RITB_2022:Q', title='Transportation Burden (Percentile)')
+#    )
+
+#transpo
 
 # ── File mappings ──────────────────────────────────────────────
 DASHBOARD_DIR = "data/dashboard"
 EXTERNAL_DIR = "data/external/dashboard"
 
-demo_options = ['Median household income','option 2']
+demo_options = ['All', 'Low Median Household Income','Hardship Index', 'Percentage of Seniors Living Alone']
 
 # ── Page config ────────────────────────────────────────────────
 st.set_page_config(page_title="Chicago Health and Pharmacy Access Dashboard", layout="wide")
@@ -55,18 +62,22 @@ demographics = st.sidebar.selectbox("Demographic", demo_options)
 #show_unchanged = st.sidebar.checkbox("Show routes with no cuts", value=True)
 #mode_filter = st.sidebar.multiselect("Mode", ["Bus", "L"], default=["Bus", "L"])
 
-# ── Column 1: Plot ────────────────────────────────────────────
-col1, col2 = st.columns(2)
+st.subheader(f"Chicago Demographics by Census Tract ({demographics})")
 
-with col1:
-    st.subheader(f"Chicago Demographics by Census Tract ({demographics})")
-
-    hist = alt.Chart(cha).mark_bar().encode(
-        alt.X('Name:O'),
-        alt.Y('INC_2020-2024:Q')
+transpo = alt.Chart(cha).mark_bar().encode(
+    alt.X('Name:O', title='Census Tract', sort = alt.EncodingSortField(field='RITB_2022', order = 'descending')),
+    alt.Y('RITB_2022:Q', title='Transportation Burden (Percentile)')
     )
 
-    hist
+# add a line showing the 50th percentile
+# source: https://stackoverflow.com/questions/77802979/how-to-draw-a-horizontal-line-at-y-0-in-an-altair-line-chart
+median = alt.Chart(pd.DataFrame({'y': [50]})).mark_rule(
+    color='red',
+    size=2
+).encode(
+    y='y:Q'
+)
 
-    st.altair_chart(hist, use_container_width=True)
+transpo = transpo + median
+transpo
 
