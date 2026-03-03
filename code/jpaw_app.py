723afa10c5a9e85b5641fd19e3f5c1c5a9520e64
Arianna Wooten <ariannasw@uchicago.edu>
2026-03-02 23:16:11 -0600

    dashboard sidebar select box works now--shows different plots based on user selection

--- code/jpaw_app.py
+++ code/jpaw_app.py
@@ -14,6 +14,22 @@ cha = pd.read_csv('/Users/ariannawooten/Downloads/final_project_AWJP_w26/data/ra
 # remove first 3 rows, which have data definitions, citations, etc.
 cha = cha.iloc[4:809]
 
+# create data subsets with different categories of census tracts
+# first convert income column to numeric data type
+cha['INC_2020-2024'] = pd.to_numeric(cha['INC_2020-2024'], downcast=None)
+
+# create df for below median income tracts
+low_inc = cha[cha['INC_2020-2024']< cha['INC_2020-2024'].median()]
+
+# create df for above median hardship index
+cha['HDX_2020-2024'] = pd.to_numeric(cha['HDX_2020-2024'], downcast=None)
+hdx = cha[cha['HDX_2020-2024'] > cha['HDX_2020-2024'].median()]
+
+# create df for above median percentage of seniors living alone
+cha['SLA-S_2020-2024'] = pd.to_numeric(cha['SLA-S_2020-2024'], downcast=None)
+senior = cha[cha['SLA-S_2020-2024'] > cha['SLA-S_2020-2024'].median()]
+
+
 # create histogram: proximity to roads, railways, and airports by census
 #hist = alt.Chart(cha).mark_bar().encode(
 #    alt.X('Name:N', title='Census Tract', 
@@ -50,34 +66,49 @@ cha = cha.iloc[4:809]
 DASHBOARD_DIR = "data/dashboard"
 EXTERNAL_DIR = "data/external/dashboard"
 
-demo_options = ['All', 'Low Median Household Income','Hardship Index', 'Percentage of Seniors Living Alone']
+#demo_options = ['All', 'Low Median Household Income','Hardship Index', 'Percentage of Seniors Living Alone']
+sample_options = {'cha': 'All', 'low_inc': 'Low Median Household Income', 'hdx': 'High Hardship Index', 'senior': 'High Percentage of Seniors Living Alone'}
+
 
 # ── Page config ────────────────────────────────────────────────
 st.set_page_config(page_title="Chicago Health and Pharmacy Access Dashboard", layout="wide")
 st.title("Chicago Health and Pharmacy Access Dashboard")
 
 # ── Sidebar controls ──────────────────────────────────────────
-demographics = st.sidebar.selectbox("Demographic", demo_options)
+demographics = st.sidebar.selectbox("Sample Options", sample_options.values())
 
 #show_unchanged = st.sidebar.checkbox("Show routes with no cuts", value=True)
 #mode_filter = st.sidebar.multiselect("Mode", ["Bus", "L"], default=["Bus", "L"])
 
-st.subheader(f"Chicago Demographics by Census Tract ({demographics})")
+st.subheader(f"Chicago Transportation Burden by Census Tract ({demographics})")
 
-transpo = alt.Chart(cha).mark_bar().encode(
+def create_plot(df=cha):
+    # determine which data subset to use
+    if demographics == 'All':
+        df = cha
+    if demographics == 'Low Median Household Income':
+        df = low_inc
+    if demographics == 'High Hardship Index':
+        df = hdx
+    if demographics == 'High Percentage of Seniors Living Alone':
+        df = senior
+    
+    transpo = alt.Chart(df).mark_bar(color='navy').encode(
     alt.X('Name:O', title='Census Tract', sort = alt.EncodingSortField(field='RITB_2022', order = 'descending')),
-    alt.Y('RITB_2022:Q', title='Transportation Burden (Percentile)')
-    )
+        alt.Y('RITB_2022:Q', title='Transportation Burden (Percentile)')
+        )
 
-# add a line showing the 50th percentile
-# source: https://stackoverflow.com/questions/77802979/how-to-draw-a-horizontal-line-at-y-0-in-an-altair-line-chart
-median = alt.Chart(pd.DataFrame({'y': [50]})).mark_rule(
-    color='red',
-    size=2
-).encode(
-    y='y:Q'
-)
+    # add a line showing the 50th percentile
+    # source: https://stackoverflow.com/questions/77802979/how-to-draw-a-horizontal-line-at-y-0-in-an-altair-line-chart
+    median = alt.Chart(pd.DataFrame({'y': [50]})).mark_rule(
+        color='red',
+        size=2
+        ).encode(
+        y='y:Q'
+        )
 
-transpo = transpo + median
-transpo
+    transpo = transpo + median
+    return(transpo)
+
+st.altair_chart(create_plot(), use_container_width=True)
 
