70608bc9d9de55c675572f40a799155837dc2424
Jessica Prus <jmprus@uchicago.edu>
2026-03-03 15:13:23 -0600

    changed merged gdf name

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -9,26 +9,46 @@ import pydeck as pdk
 data_path = 'C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Pharmacy_Status_-_Historical_20260302.csv'
 df_pharm = pd.read_csv(data_path)
 
-
-# explore data
-df_pharm.shape
-df_pharm.head()
-
 # make pharmacy names in all caps
 df_pharm['Pharmacy Name'] = df_pharm['Pharmacy Name'].str.upper()
 
+# create mapping for pharmacy status
+mapping = {
+    'OPEN':'Open',
+    'CLOSED': 'Closed',
+    'Permanently closed': 'Closed',
+    'closed': 'Closed',
+    'open': 'Open',
+    'permanently closed': 'Closed',
+    'unknown': 'Unknown'
+}
 
-### CHICAGO CENSUS DATA
+# recode the status column
+df_pharm['Status'] = df_pharm['Status'].replace(mapping)
+
+
+#################
+### CHICAGO 2000 CENSUS DATA
 
 # load census tract gdf
 census_gpd = gpd.read_file('C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Census_Tracts_20260302.geojson')
 
 
+#################
+### CHICAGO 2010 CENSUS DATA
+
+# load census tract gdf
+census_gpd = gpd.read_file('C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Census_Tracts_20260302.geojson')
+
+
+
+#################
+### POPULATION DATA
+
 # load population csv
 pop_csv = pd.read_csv('C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Population_by_2010_Census_Block_20260302.csv')
 
-
-
+#################
 ### CHICAGO HEALTH ATLAS DATA
 
 #load data
@@ -37,3 +57,54 @@ cha = pd.read_csv(cha_datapath) #chicago health atlas data (2020)
 
 # remove first 3 rows, which have data definitions, citations, etc.
 cha = cha.iloc[4:809]
+
+#################
+### COMBINING PHARMACIES AND CENSUS BLOCKS
+
+# load pharmacies as a geodataframe
+
+# drop nas
+df_pharm = df_pharm.dropna(subset=['New Georeferenced Column'])
+
+# drop na strings
+df_pharm = df_pharm[
+    df_pharm['New Georeferenced Column']
+    .str.lower()
+    .ne('nan')
+]
+
+# convert wkt to a geometry
+df_pharm['geometry'] = gpd.GeoSeries.from_wkt(
+    df_pharm['New Georeferenced Column'],
+    on_invalid='ignore'
+)
+
+# drop failed parses
+df_pharm = df_pharm.dropna(subset=['geometry'])
+
+# create geodataframe
+pharm_gdf = gpd.GeoDataFrame(
+    df_pharm,
+    geometry='geometry',
+    crs='EPSG:4326'
+)
+
+# sanity check
+print(df_pharm.shape)
+print(df_pharm.head())
+
+# join gdfs
+combined1_gdf = gpd.sjoin(pharm_gdf, census_2010_gdf, 
+    how='left',
+    predicate='within')
+
+# check object type
+print('Object type:', type(combined1_gdf))
+
+# check geometry type
+print('\nGeometry type:', combined1_gdf.geom_type.value_counts())
+
+
+
+
+
