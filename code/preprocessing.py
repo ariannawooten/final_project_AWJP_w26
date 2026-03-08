5b402f3611019b880fabfe37269d205a3cb6d4e6
Jessica Prus <jmprus@uchicago.edu>
2026-03-08 13:19:05 -0500

    add universal data paths

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -4,10 +4,14 @@ from pathlib import Path
 from shapely import wkt
 import pydeck as pdk
 
+script_dir = Path(__file__).parent
+
+
+
 ### PHARMACY DATA
 # load data
-data_path = 'C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Pharmacy_Status_-_Historical_20260302.csv'
-df_pharm = pd.read_csv(data_path)
+pharmacy = script_dir / '../data/raw-data/Pharmacy_Status_-_Historical_20260302.csv'
+df_pharm = pd.read_csv(pharmacy)
 
 # make pharmacy names in all caps
 df_pharm['Pharmacy Name'] = df_pharm['Pharmacy Name'].str.upper()
@@ -31,29 +35,33 @@ df_pharm['Status'] = df_pharm['Status'].replace(mapping)
 ### CHICAGO 2000 CENSUS DATA
 
 # load census tract gdf
-census_2000_gpd = gpd.read_file('C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Census_Tracts_20260302.geojson')
+census_2000 = script_dir / '../data/raw-data/Census_Tracts_20260302.geojson'
+census_2000_gpd = gpd.read_file(census_2000)
 
 
 #################
 ### CHICAGO 2010 CENSUS DATA
 
 # load census tract gdf
-census_2010_gpd = gpd.read_file('C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\CensusTractsTIGER2010_20260303.geojson')
-
+census_2010 = script_dir / '../data/raw-data/CensusTractsTIGER2010_20260303.geojson'
+census_2010_gpd = gpd.read_file(census_2000)
 
 
 #################
 ### POPULATION DATA
 
 # load population csv
-pop_csv = pd.read_csv('C:\\Users\\blueb\\final_project_AWJP_w26\\data\\raw-data\\Population_by_2010_Census_Block_20260302.csv')
+pop = script_dir / '../data/raw-data/Population_by_2010_Census_Block_20260302.csv'
+pop_csv = pd.read_csv(pop)
 
 #################
 ### CHICAGO HEALTH ATLAS DATA
 
 #load data
-cha_datapath = '/Users/ariannawooten/Downloads/final_project_AWJP_w26/data/raw-data/Chicago Health Atlas Data Download - Census Tracts.csv' #chicago health atlas data (2020)'
-cha = pd.read_csv(cha_datapath) #chicago health atlas data (2020)
+cha_datapath = script_dir / '../data/raw-data/Chicago Health Atlas Data Download - Census Tracts.csv'
+cha = pd.read_csv(cha_datapath)
+
+
 
 # remove first 3 rows, which have data definitions, citations, etc.
 cha = cha.iloc[4:809]
