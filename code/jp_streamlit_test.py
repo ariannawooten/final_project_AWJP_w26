31bafd1a7bcdccf667ed621f82f70e13077e9c61
Jessica Prus <jmprus@uchicago.edu>
2026-03-08 12:48:15 -0500

    add chicago map

--- code/jp_streamlit_test.py
+++ code/jp_streamlit_test.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
 st.write('hello world')
+
+st.title('Chicago Map')
+
+df = pd.DataFrame(
+    np.random.randn(100, 2) / [100,100] + [41.8781, -87.6298],
+    columns=['lat', 'lon']
+)
+st.map(df)
 
